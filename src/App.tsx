42a5e325563b96a4527ad232fdb392fe45177e1c
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-02-22 16:13:19 +0700

    feat: update main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { Box, Button, Flex, Heading, HStack, Spacer, Text } from '@chakra-ui/react';
+// Libraries
+import { Flex, Heading } from '@chakra-ui/react';
 
 function App() {
   return (
     <Flex as='nav' p='10px' alignItems='center'>
-      <Heading as='h1'>Logo</Heading>
-      <Spacer />
-      <HStack spacing='20px'>
-        <Box bg='gray.200' p='10px' borderRadius='50%'>
-          N
-        </Box>
-        <Text>Some text here</Text>
-        <Button colorScheme='blue'>Logout</Button>
-      </HStack>
+      <Heading as='h1'>Welcome, ISkillz</Heading>
     </Flex>
   );
 }
