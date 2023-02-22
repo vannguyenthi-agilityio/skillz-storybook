2b8c40999ab8d05ba22772e61e72f88612c258c7
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-02-22 16:18:45 +0700

    feat: add comment import and update script

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
-import { ChakraProvider } from '@chakra-ui/react';
+// Libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+
+// Components
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
