7684f0adb0158b3911d768e653fe252771078221
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-02-24 17:20:52 +0700

    feat: add icons for buttons

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,5 +1,8 @@
 const ASSETS = {
   ICON_CHARITY: '../src/assets/icons/charity.svg',
+  ICON_MASK: '../src/assets/icons/mask.svg',
+  ICON_ADD: '../src/assets/icons/add.svg',
+  ICON_EDIT: '../src/assets/icons/edit.svg',
 };
 
 export default ASSETS;
