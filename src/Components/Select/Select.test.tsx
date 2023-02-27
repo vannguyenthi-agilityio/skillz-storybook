ce140aaac3076a857b97cb168a1e11893434d77a
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-02-27 11:08:23 +0700

    feat: #4 test select component

---
+++ src/Components/Select/Select.test.tsx
@@ -0,0 +1,38 @@
+//Libraries
+import { fireEvent, render, screen } from '@testing-library/react';
+import Select from '.';
+
+const mockOptions = [
+  { label: 'Option 1', value: '1' },
+  { label: 'Option 2', value: '2' },
+  { label: 'Option 3', value: '3' },
+];
+
+describe('Select render', () => {
+  test('should render select comnponent', () => {
+    render(<Select options={mockOptions} value={mockOptions[0].value} label='Select' onChange={() => {}} />);
+
+    const select = screen.getByTestId('select');
+
+    expect(select).toBeInTheDocument();
+  });
+
+  test('should render without label', () => {
+    render(<Select options={mockOptions} value={mockOptions[0].value} onChange={() => {}} />);
+
+    const select = screen.queryByText('Select');
+
+    expect(select).toBeNull();
+  });
+
+  test('should simulate onChange event', () => {
+    const mockOnChange = jest.fn();
+
+    render(<Select options={mockOptions} value={mockOptions[0].value} label='Select' onChange={mockOnChange} />);
+
+    const select = screen.getByTestId('select');
+    fireEvent.change(select);
+
+    expect(mockOnChange).toHaveBeenCalled();
+  });
+});
