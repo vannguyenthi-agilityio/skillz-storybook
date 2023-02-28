fd5c179e2fd2575998aff78b4b2791caca2cdcee
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-02-28 07:40:38 +0700

    feat: creat convert px to rem

---
+++ src/helpers/common.ts
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param baseSize 16px
+ * @returns "1rem"
+ */
+export const convertPxToRem = (pxValue: number ) => {
+  let baseSize = window.getComputedStyle(document.getElementsByTagName('html')[0]).getPropertyValue('font-size').slice(0, 2);
+  return `${pxValue/Number(baseSize)}rem`;
+};
